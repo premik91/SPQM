affc5ea8b7647d66b3f6c3a5d94808142e5a7769
premik91 <i@premik91.com>
2013-07-13 01:02:45 +0200

    Comment fix.

--- SPQM/static/js/main.js
+++ SPQM/static/js/main.js
@@ -2,6 +2,6 @@ var $ = jQuery.noConflict();
 $(document).ready(function () {
     $('.start-tooltip').tooltip();
 
-    // Hide message after 10 seconds
+    // Hide message after 5 seconds
     $('ul.messages').delay(5000).fadeOut('slow');
 });
